96682d5d44253061a97d3b796ca605132f34d2b9
Vladymyr <85882951+Syglob@users.noreply.github.com>
2021-12-08 16:58:47 +0200

    pree BETA

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const DEBOUNCE_DELAY = 300;
 const searchInput = document.getElementById('search-box');
 const countryList = document.querySelector('.country-list');
 const countryInfo = document.querySelector('.country-info');
-// import fetchCountries from '/fetchCountries';
+//
 function fetchCountries(nameCountry) {
   return fetch(
     `https://restcountries.com/v3.1/name/${nameCountry}?fields=name,capital,population,flags,languages`,
@@ -16,7 +16,6 @@ function fetchCountries(nameCountry) {
       if (!res.ok) {
         throw new Error('Error fetching countries');
       }
-      // Notify.success('Страны найдены');
       return res.json();
     })
     .then(data => {
@@ -25,19 +24,20 @@ function fetchCountries(nameCountry) {
       return countries;
     })
     .catch(err => {
-      Notify.failure('Страны не найдены');
+      Notify.failure('Nothing found');
+      countryList.innerHTML = '';
+      countryInfo.innerHTML = '';
       console.log('fetching err', err);
     });
 }
-
+//
 searchInput.addEventListener('input', debounce(getDataInput, DEBOUNCE_DELAY));
-
+//
 function getDataInput(e) {
   const searchValue = e.target.value;
   if (searchValue.length === 0) {
     countryList.innerHTML = '';
     countryInfo.innerHTML = '';
-    // Notify.info('Введите название страны');
     return;
   }
   if (searchValue.length < 3) {
@@ -46,18 +46,19 @@ function getDataInput(e) {
       .then(data => {
         const countries = data;
         const countryListItems = countries.map(country => {
-          return `<p class ="info">
-              <span class="country-flag"><img width = "100px" src="${country.flags.svg}" alt="country flag"></span>
+          return `<li class ="info">
+              <span class="country-flag"><img width = "40px" src="${country.flags.svg}" alt="country flag"></span>
               <span class="country-name">${country.name.official}</span>
-          </p>`;
+          </li>`;
         });
-        countryInfo.innerHTML = countryListItems.join('');
+        countryList.innerHTML = countryListItems.join('');
       })
       .catch(err => {
         console.log('beda', err);
       });
     return;
   }
+  countryList.innerHTML = '';
   fetchCountries(searchValue)
     .then(data => {
       const countries = data;
@@ -74,6 +75,7 @@ function getDataInput(e) {
       });
       countryInfo.innerHTML = countryListItems.join('');
     })
+
     .catch(err => {
       console.log('beda', err);
     });
